3b08d3b8b8f6bbf3e9019485c03d3deaf96a9866
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-11-28 13:28:04 +0100

    create new folders to test all created classificators

---
+++ src/fake_news_detector/runners/scp_lda.py
@@ -0,0 +1,21 @@
+from src.fake_news_detector.classificators.lda import LDA
+from src.fake_news_detector.dataset import dataset
+
+# OBJECTIVE: Classificate by title
+
+# 1. Get dataset
+data = dataset.modelate_dataset()
+
+# 2. Clean title
+
+
+# 3. Vectorize
+
+
+# 4. Create LDA classificator
+
+
+# 5. Predict
+
+
+# 6. Get results
