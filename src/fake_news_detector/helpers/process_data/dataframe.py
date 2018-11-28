5563cb8e8b2b584c3e095182d9fbfcb5b3676652
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-11-28 12:20:19 +0100

    Adding helpers

---
+++ src/fake_news_detector/helpers/process_data/dataframe.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import json
+
+from src.utils import log
+
+# File that include function to manage pandas dataframe
+
+# Pre:  JSON content
+# Post: Pandas dataframe with content input 
+def get_dataframe_from_json(content):
+    try:
+        log.info('Generating DataFrame...')
+        df = pd.DataFrame(data=content['articles'])
+        log.info('Done')
+        print(get_columns_name(df))
+        return df
+    except ValueError as err:
+        log.error('|DATAFRAME.py| - Can\'t create pandas dataframe')
+        log.error(err)
+
+
+# UTILS PANDAS FUNCTIONS
+
+def get_columns_name(df):
+    return df.dtypes
+
+# rows x cols
+def get_matrix_size(df):
+    return df.shape
---
+++ src/fake_news_detector/helpers/read_data/io.py
@@ -0,0 +1,26 @@
+from app.utils import log
+import json
+
+# Read JSON file
+# Pre: JSON file's *path*
+# Post: Content of JSON path
+def read_json_file(path):
+    try:
+        log.info('Reading ' + path + ' file ...')
+        with open(path, 'r') as f:
+            log.info(path + ' file opened.')
+            return json.load(f)
+    except IOError as err: # whatever reader errors you care about
+        log.error('|IOError| - File not found or couldn\'t be open')
+
+# Write dictionary or JSON in JSON file
+# Pre: *path* where file is going to be store, *content* dictionary's content
+# Post: File in path *path* is gonna be created.
+def write_json_file(path, content):
+    try:
+        log.info('Opening ' + path + ' file ...')
+        with open(path, 'w') as outfile:
+            json.dump(content, outfile)
+            log.info(path + ' file writed.')
+    except IOError as err: # whatever reader errors you care about
+        log.error('|IOError| - File not found or couldn\'t be open')
