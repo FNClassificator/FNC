0639823bdb754357a4ba06999c4d6a1d46471000
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-11-30 16:07:07 +0100

    Implement more title test classificator with LDA

--- src/fake_news_detector/runners/scp_lda.py
+++ src/fake_news_detector/runners/scp_lda.py
@@ -1,4 +1,5 @@
 from src.fake_news_detector.helpers.process_data import pre_process
+from src.fake_news_detector.helpers.nlp import feature_extractions as fe
 
 # OBJECTIVE: Classificate by title
 
@@ -6,7 +7,19 @@ from src.fake_news_detector.helpers.process_data import pre_process
 dataset = pre_process.modelate_dataset()
 
 # 2. Clean title
-clean_title(dataset)
+pre_process.tokenize_by_word_and_clean(dataset, 'title')
+print(dataset["title_token_clean"][0])
+print(dataset["title"][0])
+
+
+dataset['title_perct_adj'] = dataset['title_token_clean'] # Copy of column
+dataset['title_perct_noun'] = dataset['title_token_clean'] # Copy of column
+for _, row in dataset.iterrows():
+    # Extract % adjectives
+    row['title_perct_adj'] = fe.count_adjectives(row['title_perct_adj'])
+    # Extract % nouns
+    row['title_perct_noun'] = fe.count_uncommon_nouns(row['title_perct_noun'])
+
 
 # 3. Vectorize
 
