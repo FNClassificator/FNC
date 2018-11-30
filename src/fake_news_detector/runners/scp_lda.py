c62236b99c41869a41df5265cb5ae19251ad1972
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-11-30 16:54:15 +0100

    Extract new features from title subj, pol, adj, and nouns

--- src/fake_news_detector/runners/scp_lda.py
+++ src/fake_news_detector/runners/scp_lda.py
@@ -11,17 +11,29 @@ pre_process.tokenize_by_word_and_clean(dataset, 'title')
 print(dataset["title_token_clean"][0])
 print(dataset["title"][0])
 
-
+# 3. Extract informations
 dataset['title_perct_adj'] = dataset['title_token_clean'] # Copy of column
 dataset['title_perct_noun'] = dataset['title_token_clean'] # Copy of column
+dataset['title_subjectivity'] =  dataset['title_token_clean']
+dataset['title_polarity'] =  dataset['title_token_clean']
 for _, row in dataset.iterrows():
     # Extract % adjectives
-    row['title_perct_adj'] = fe.count_adjectives(row['title_perct_adj'])
+    row['title_perct_adj'] = fe.count_adjectives(row['title_token_clean'])
     # Extract % nouns
-    row['title_perct_noun'] = fe.count_uncommon_nouns(row['title_perct_noun'])
+    row['title_perct_noun'] = fe.count_common_nouns(row['title_token_clean'])
+    # Sentiment 
+    sentiment = fe.get_sentiment(row['title'])
+    # Subjectivity
+    row['title_subjectivity'] = sentiment.subjectivity
+    # Polarity
+    row['title_polarity'] = sentiment.polarity
 
+print(dataset["title_perct_adj"][0])
+print(dataset["title_perct_noun"][0])
+print(dataset["title_subjectivity"][0])
+print(dataset["title_polarity"][0])
 
-# 3. Vectorize
+# 3. Normalize
 
 
 # 4. Create LDA classificator
