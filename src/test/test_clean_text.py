b58c67a921e4700a260c62b4613dc605babcdf01
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-12-02 16:47:28 +0100

    Add tests files

---
+++ src/test/test_clean_text.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestCleanText(unittest.TestCase):
+
+    def test_tokenize_by_sentence(self):
+        return
+
+
+    def test_tokenize_by_word(self):
+        return
+
+
+    def test_lemmatize(self):
+        return
+    
+    
+    def test_steamming(self):
+        return
+
+if __name__ == '__main__':
+    unittest.main()
