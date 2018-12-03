fdffdb42cb4c3e4fdc6d54da65752034e7904202
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-12-03 23:17:11 +0100

    Add new css classes from news webs

--- src/scripts/scrapper/css_attributes.py
+++ src/scripts/scrapper/css_attributes.py
@@ -44,3 +44,105 @@ MARCA = {
         "attr": {"itemprop": "articleBody"}
     }
 }
+
+MEDITERRANEO = {
+    "title": {
+        "name": "h2",
+        "attr": {"class": "contentheading"}
+    },
+    "subtitle":{
+        "name": "h2",
+        "attr": { "class": "contentheading"}
+    },
+    "text": {
+        "name": "div",
+        "attr": { "id": "medite-articulo"}
+    }
+}
+
+
+ALERTADIGITAL = {
+    "title": {
+        "name": "h2"
+    },
+    "subtitle":{
+        "name": "h2"
+    },
+    "text": {
+        "name": "div",
+        "attr": { "class": "entry"}
+    }
+}
+
+CATALADIGITAL = {
+    "title": {
+        "name": "h1",
+        "attr": { "class": "post-title"}
+    },
+    "subtitle":{
+        "name": "h1",
+        "attr": { "class": "post-title"}
+    },
+    "text": {
+        "name": "div",
+        "attr": { "class": "post-content"}
+    }
+}
+
+EUROPAPRESS = {
+    "title": {
+        "name": "h1",
+        "attr": { "class": "titular" }
+    },
+    "subtitle":{
+        "name": "h1",
+        "attr": { "class": "titular"}
+    },
+    "text": {
+        "name": "div",
+        "attr": { "itemprop": "articleBody"}
+    }
+}
+
+ESDIARIO = {
+    "title": {
+        "name": "h1",
+        "attr": {"class": "tit_art_principal"}
+    },
+    "subtitle":{
+        "name": "h3",
+        "attr": { "itemprop": "description"}
+    },
+    "text": {
+        "name": "div",
+        "attr": { "class": "CuerpoNoticia"}
+    }
+}
+
+GOMERAACTUALIDAD = {
+    "title": {
+        "name": "h1",
+        "attr": {"class": "title"}
+    },
+    "subtitle":{
+        "name": "div",
+        "attr": { "class": "summary"}
+    },
+    "text": {
+        "name": "div",
+        "attr": { "class": "body"}
+    }
+}
+
+PERIODISTADIGITAL = {
+    "title": {
+        "name": "h1"
+    },
+    "subtitle":{
+        "name": "h1"
+    },
+    "text": {
+        "name": "div",
+        "attr": { "class": "text-block"}
+    }
+}
