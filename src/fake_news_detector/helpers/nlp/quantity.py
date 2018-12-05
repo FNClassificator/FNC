dba64315417fa7349fc45665b33604f2d56c4154
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-12-05 12:53:54 +0100

    Add new functions without implementing them

--- src/fake_news_detector/helpers/nlp/quantity.py
+++ src/fake_news_detector/helpers/nlp/quantity.py
@@ -6,6 +6,7 @@
 def n_words(text_tokens):
     return len(text_tokens)
 
+
 def perct_noun_words(tag_tokens):
     total = 0
     for token, tag in tag_tokens:
@@ -42,10 +43,12 @@ def mean_word_per_sent(sent_tokens):
         t_sum += len(sent)
     return t_sum / len(sent_tokens)
 
+
 def mean_clause_per_sent(sent_tokens):
     t_sum = 0
     # call function to split by clauses
     return t_sum
+
 
 def mean_characters_per_word(texts_tokens):
     t_sum = 0
@@ -56,14 +59,92 @@ def mean_characters_per_word(texts_tokens):
             t_sum += len(word)
     return t_sum / total
 
+
 def mean_punctuation_per_sent(sent_tokens):
     # TODO
     return 0
+
+
+# U N C E R T A I N T Y
+
+def pert_modal_verbs(text_tokens):
+    return
+
+
+def pert_certainty_terms(text_tokens):
+    return
+
+
+def pert_generalizing_terms(text_tokens):
+    return
+
+
+def pert_tentative_terms(text_tokens):
+    return
+
+
+def pert_numbers_and_quantifiers(text_tokens):
+    return
+
+
+def pert_question_marks(text_tokens):
+    return
 
 # T Y P E   O F   V E R B S
 
 def pert_subjective_verbs(texts_tokens):
     total_verbs = 0
     total_subj_verbs = 0
-    for sent in texts_tokens:
-        
+    return
+
+
+def pert_report_verbs(text_tokens):
+    return
+
+
+def pert_factive_verbs(text_tokens):
+    return
+
+
+def pert_imperative_commands(text_tokens):
+    return
+
+# S E N T I M E N T
+
+def pert_positive_words(text_tokens):
+    return
+
+
+def pert_negative_words(text_tokens):
+    return
+
+def pert_emotional_phrases(text_tokens):
+    return
+
+
+# D I V E R S I T Y
+
+def lexical_diversity(text_tokens):
+    return
+
+
+def reduncancy(text_tokens):
+    return
+
+
+# NON - I M M E D I A C Y
+
+def pert_passive_voice(text_tokens):
+    return
+
+def pert_retorical_questions(text_tokens):
+    return
+
+def pert_self_reference(text_tokens):
+    return
+
+def pert_group_reference(text_tokens):
+    return
+
+def perct_other_reference(text_tokens):
+    return
