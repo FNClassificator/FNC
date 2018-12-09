a7426c1fd335740d73e33c8d0661e28d1fb1d949
elena20ruiz <elena.ruiz.bdn@gmail.com>
2018-12-09 15:33:34 +0100

    Implement all scripts tu run classificators and subclassificators

--- src/fake_news_detector/runners/main_classificator.py
+++ src/fake_news_detector/runners/main_classificator.py
@@ -6,7 +6,7 @@ from src.fake_news_detector.classification import sub_classifications as sc
 
 title_dataset = io.read_json_file('/home/elenaruiz/Documents/TFG/FNC/src/data/dataset_title.json')
 similarity_dataset = io.read_json_file('/home/elenaruiz/Documents/TFG/FNC/src/data/dataset_similarity.json')
-text_dataset = io.read_json_file('/home/elenaruiz/Documents/TFG/FNC/src/data/dataset.json')
+text_dataset = io.read_json_file('/home/elenaruiz/Documents/TFG/FNC/src/data/dataset_text.json')
 
 # Datasets
 df_title = pd.DataFrame(data=title_dataset['articles'])
@@ -14,22 +14,52 @@ df_similarity = pd.DataFrame(data=similarity_dataset['articles'])
 df_text = pd.DataFrame(data=text_dataset['articles'])
 
 # Predict TITLE
-y_title = sc.get_title_prediction(df_title, 'LR')
+y_title = sc.get_title_prediction(df_title, 'LR', False)
 print(y_title)
 
 # Predict SIMILARITY
-y_similarity = sc.get_similarity_prediction(df_similarity, 'LR')
+y_similarity = sc.get_similarity_prediction(df_similarity, 'LR', False)
 print(y_similarity)
 
 # Predict TEXT
-y_text = sc.get_text_prediction(df_text, 'LR')
+y_text = sc.get_text_prediction(df_text, 'LR', False)
 print(y_text)
 
 # Join results in a dataset
-final_dataset  = []
-final_dataset.append(y_title)
-final_dataset.append(y_similarity)
-final_dataset.append(y_text)
-df_final = pd.DataFrame(data=final_dataset)
+data_final = {
+    'articles': []
+}
+for i in range(0,len(y_title)):
+    aux = { }
+    aux['title'] = y_title[i]
+    aux['similarity'] = y_similarity[i]
+    aux['text'] = y_text[i]
+    aux['fake'] = title_dataset['articles'][i]['fake']
+    data_final['articles'].append(aux)
+
+df_final = pd.DataFrame(data=data_final['articles'])
+
 
 # Train with some techniques
+model_type = 'LR'
+pred = sc.get_main_prediction(df_final, model_type, True)
+
+
+model_type = 'DTC'
+pred = sc.get_main_prediction(df_final, model_type, True)
+
+
+model_type = 'KNC'
+pred = sc.get_main_prediction(df_final, model_type, True)
+
+
+model_type = 'LDA'
+pred = sc.get_main_prediction(df_final, model_type, True)
+
+
+model_type = 'GNB'
+pred = sc.get_main_prediction(df_final, model_type, True)
+
+
+model_type = 'SVC'
+pred = sc.get_main_prediction(df_final, model_type, True)
--- src/fake_news_detector/runners/title_classificator.py
+++ src/fake_news_detector/runners/title_classificator.py
@@ -1,85 +1,34 @@
 import numpy as np
 import pandas as pd
 
-#ML libraries
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-
-# Classifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
 from src.utils import io
+from src.fake_news_detector.classification.sub_classifications import get_title_prediction
 
-articles = io.read_json_file('/home/elenaruiz/Documents/TFG/FNC/src/data/dataset_title.json')
-df = pd.DataFrame(data=articles['articles'])
+if __name__ == "__main__":
 
-# Compare fake with no fake
-fake_df = df.loc[df['fake'] == 1]
-real_df = df.loc[df['fake'] == 0]
-
-# X get important variables
-X = df[['sentiment','title_adj_words','title_verbs_words']]
-y = df['fake']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-scaler = MinMaxScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+    articles = io.read_json_file('/home/elenaruiz/Documents/TFG/FNC/src/data/dataset_title.json')
+    df = pd.DataFrame(data=articles['articles'])
 
 
-# LOGISTIC REGRESION
-logreg = LogisticRegression()
-logreg.fit(X_train, y_train)
-print('Accuracy of Logistic regression classifier on training set: {:.2f}'
-     .format(logreg.score(X_train, y_train)))
-print('Accuracy of Logistic regression classifier on test set: {:.2f}'
-     .format(logreg.score(X_test, y_test)))
+    model_type = 'LR'
+    pred = get_title_prediction(df, model_type, True)
 
 
-# DECISION TREE
-clf = DecisionTreeClassifier().fit(X_train, y_train)
-print('Accuracy of Decision Tree classifier on training set: {:.2f}'
-     .format(clf.score(X_train, y_train)))
-print('Accuracy of Decision Tree classifier on test set: {:.2f}'
-     .format(clf.score(X_test, y_test)))
+    model_type = 'DTC'
+    pred = get_title_prediction(df, model_type, True)
 
 
-# KNN
-knn = KNeighborsClassifier()
-knn.fit(X_train, y_train)
-print('Accuracy of K-NN classifier on training set: {:.2f}'
-     .format(knn.score(X_train, y_train)))
-print('Accuracy of K-NN classifier on test set: {:.2f}'
-     .format(knn.score(X_test, y_test)))
+    model_type = 'KNC'
+    pred = get_title_prediction(df, model_type, True)
 
 
-# LDA
-lda = LinearDiscriminantAnalysis()
-lda.fit(X_train, y_train)
-print('Accuracy of LDA classifier on training set: {:.2f}'
-     .format(lda.score(X_train, y_train)))
-print('Accuracy of LDA classifier on test set: {:.2f}'
-     .format(lda.score(X_test, y_test)))
+    model_type = 'LDA'
+    pred = get_title_prediction(df, model_type, True)
 
 
-#Gaussian
-gnb = GaussianNB()
-gnb.fit(X_train, y_train)
-print('Accuracy of GNB classifier on training set: {:.2f}'
-     .format(gnb.score(X_train, y_train)))
-print('Accuracy of GNB classifier on test set: {:.2f}'
-     .format(gnb.score(X_test, y_test)))
+    model_type = 'GNB'
+    pred = get_title_prediction(df, model_type, True)
 
 
-# SVC
-svm = SVC()
-svm.fit(X_train, y_train)
-print('Accuracy of SVM classifier on training set: {:.2f}'
-     .format(svm.score(X_train, y_train)))
-print('Accuracy of SVM classifier on test set: {:.2f}'
-     .format(svm.score(X_test, y_test)))
+    model_type = 'SVC'
+    pred = get_title_prediction(df, model_type, True)
